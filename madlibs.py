ac8af4aae11b0ff5d38ce168c1525d568d6ea187
gowry lakshmi <sgowrylakshmi@gmail.com>
2023-12-15 20:07:14 +0000

    first commit

---
+++ madlibs.py
@@ -0,0 +1,6 @@
+adjective = input("Adjective: ")
+adjective2 = input("Adjective2: ")
+verb = input("verb: ")
+madlib = f"I love to code because it is so {adjective}.\
+    It makes me want to jump and {adjective2}.Happy to {verb}"
+print(madlib)
